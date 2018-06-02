8a5fd60c0225e5ea4f3595fc0d1dc5fdad4e65f4
Buster01 <mario.oschatz@computer-kern.de>
2018-06-02 23:09:19 +0200

    -

--- VideoEncoder/My Project/AssemblyInfo.vb
+++ VideoEncoder/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.2.0.2")>
-<Assembly: AssemblyFileVersion("0.2.0.2")>
+<Assembly: AssemblyVersion("0.2.1.2")>
+<Assembly: AssemblyFileVersion("0.2.1.2")>
